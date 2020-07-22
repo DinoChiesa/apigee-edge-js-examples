c6fc5df57e3f9f5bfcce52a5bb5cdbd70c404f6b
Dino Chiesa <dchiesa@google.com>
2020-07-22 14:51:45 -0700

    add tolerance of runtime errors when querying
     - if "API Proxy revision xx does not exist" happens,
       this should not cause the script to fail.
    
     - also add in a parameter -P that allows people to filter
       the list of proxies to scan.

--- findVhostsForDeployedProxies.js
+++ findVhostsForDeployedProxies.js
@@ -1,5 +1,5 @@
 #! /usr/local/bin/node
-/*jslint node:true, esversion:6 */
+/*jslint node:true, esversion:9 */
 // findVhostsForDeployedProxies.js
 // ------------------------------------------------------------------
 // In Apigee Edge, for all proxies, find the latest deployed revision and
@@ -10,7 +10,7 @@
 //  node ./examples/findVhostsForDeployedProxies.js -n -v -o myorgname -R default
 //
 //
-// Copyright 2018-2019 Google LLC.
+// Copyright 2018-2020 Google LLC.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -24,7 +24,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 //
-// last saved: <2019-February-11 13:01:26>
+// last saved: <2020-July-22 14:51:10>
 
 const async      = require('async'),
       edgejs     = require('apigee-edge-js'),
@@ -33,12 +33,13 @@ const async      = require('async'),
       sprintf    = require('sprintf-js').sprintf,
       Getopt     = require('node-getopt'),
       merge      = require('merge'),
-      version    = '20190211-1300',
+      version    = '20200722-1435',
       getopt     = new Getopt(common.commonOptions.concat([
-        ['R' , 'regexp=ARG', 'Optional. List proxies with vhosts matching this regexp.']
+        ['R' , 'regexp=ARG', 'Optional. List proxies with vhosts matching this regexp.'],
+        ['P' , 'proxyregexp=ARG', 'Optional. consider only proxies with names matching this regexp.']
       ])).bindHelp();
 
-var gRegexp;
+var gVhostRegexp;
 
 // ========================================================
 
@@ -59,9 +60,8 @@ function handleError(e) {
 
 function getVhostForProxyEndpoint(org, proxy, revision) {
   return function(endpoint, cb) {
-    org.proxies.getEndpoint({apiproxy:proxy, revision:revision, endpoint:endpoint}, function(e, result){
-      return cb(e, { name:endpoint, virtualHosts : result.connection.virtualHost});
-    });
+    org.proxies.getEndpoint({apiproxy:proxy, revision:revision, endpoint:endpoint},
+                            (e, result) => cb(e, { name:endpoint, virtualHosts : result.connection.virtualHost}) );
   };
 }
 
@@ -70,28 +70,29 @@ function checkDeployedRevisionOfProxy(org, proxyName){
     if ( ! revEnvironment.environments || revEnvironment.environments.length == 0) {
       cb(null, []);
     }
-    org.proxies.getProxyEndpoints({apiproxy:proxyName, revision: revEnvironment.revision}, function(e, result){
-      handleError(e);
-      async.mapSeries(result, getVhostForProxyEndpoint(org, proxyName, revEnvironment.revision), function(e, result){
-        var endpoints = result;
+    org.proxies.getProxyEndpoints({apiproxy:proxyName, revision: revEnvironment.revision}, (e, result) => {
+      if (e) {
+        console.warn(`unable to retrieve deployed revision ${proxyName}:${revEnvironment.revision}`);
+        return cb(null, []);
+      }
+
+      async.mapSeries(result, getVhostForProxyEndpoint(org, proxyName, revEnvironment.revision), (e, endpoints) => {
         if (opt.options.regexp) {
-          endpoints = result.filter( endpt => {
-            var matchingVhosts = endpt.virtualHosts.filter( item => gRegexp.test(item) );
-            return matchingVhosts.length>0;
-          });
+          endpoints = endpoints.filter( endpt =>
+                                        endpt.virtualHosts.filter( item => gVhostRegexp.test(item) ).length>0 );
         }
-        return cb(e, (endpoints.length>0)? {name: revEnvironment.revision, environments: revEnvironment.environments, endpoints:result} : null);
+        return cb(e, (endpoints.length>0)? {name: revEnvironment.revision, environments: revEnvironment.environments, endpoints} : null);
       });
     });
   };
 }
 
 function remap(proxyDeployment) {
-  var item = { proxyname : proxyDeployment.name, deployments: [] };
+  let item = { proxyname : proxyDeployment.name, deployments: [] };
   proxyDeployment.environment.forEach( environmentDeployment => {
     //console.log(JSON.stringify(environmentDeployment, null, 2) + '\n');
     environmentDeployment.revision.forEach ( rev => {
-      var list = item.deployments.find( x => x.revision == rev.name );
+      let list = item.deployments.find( x => x.revision == rev.name );
       if ( ! list ) {
         list = { revision: rev.name, environments: []} ;
         item.deployments.push(list);
@@ -105,39 +106,53 @@ function remap(proxyDeployment) {
 
 function findLatestDeployments(org) {
   return function(itemName, cb) {
-    org.proxies.getDeployments({ name: itemName }, function(e, result) {
-      handleError(e);
-      result = remap(result);
-      // if (opt.options.verbose) {
-      //   common.logWrite('deployments: %s', JSON.stringify(result));
-      // }
-      async.mapSeries(result.deployments, checkDeployedRevisionOfProxy(org, itemName), function(e, result){
-        var revisions = result.filter( item => item );
-        return cb(e, (revisions.length>0)? {proxy:itemName, revisions:result }: null);
+    org.proxies.getDeployments({ name: itemName })
+      .then( result => remap(result) )
+      .then( result => {
+        // if (opt.options.verbose) {
+        //   common.logWrite('deployments: %s', JSON.stringify(result));
+        // }
+        return async.mapSeries(result.deployments, checkDeployedRevisionOfProxy(org, itemName), (e, result) => {
+          let revisions = result.filter( item => item );
+          return cb(e, (revisions.length>0)? {proxy:itemName, revisions }: null);
+        });
+      })
+      .catch(e => {
+        console.warn(`unable to retrieve deployments for proxy ${itemName}`);
+        return cb(null, null);
       });
-    });
   };
 }
 
 common.verifyCommonRequiredParameters(opt.options, getopt);
 
 if (opt.options.regexp) {
-  gRegexp = new RegExp(opt.options.regexp);
+  gVhostRegexp = new RegExp(opt.options.regexp);
 }
-apigeeEdge.connect(common.optToOptions(opt), function(e, org){
-  handleError(e);
-
-  var readOptions = {};
-  org.proxies.get(readOptions, function(e, results) {
-    if (opt.options.verbose) {
-      common.logWrite('proxies: %s', JSON.stringify(results));
-    }
-    if (results.length > 0) {
-      async.mapSeries(results, findLatestDeployments(org), function(e, results){
-        handleError(e);
-        results = results.filter( item => item ); // null item means no regexp match for that proxy
-        console.log(JSON.stringify({results, count:results.length}, null, 2) + '\n');
-      });
-    }
-  });
-});
+apigeeEdge
+  .connect(common.optToOptions(opt))
+  .then( org =>
+         org.proxies.get({})
+         .then( results => {
+           if (opt.options.verbose) {
+             common.logWrite('proxies: %s', JSON.stringify(results));
+           }
+           if (results.length > 0) {
+             // optionally filter the list of proxies to scan
+             if (opt.options.proxyregexp) {
+               let re1 =  new RegExp(opt.options.proxyregexp);
+               results = results.filter( item => re1.test(item));
+               if (opt.options.verbose) {
+                 common.logWrite('filtered: %s', JSON.stringify(results));
+               }
+             }
+             // scan them
+             return async.mapSeries(results, findLatestDeployments(org), (e, results) => {
+               handleError(e);
+               results = results.filter( item => item ); // null item means no regexp match for that proxy
+               console.log(JSON.stringify({results, count:results.length}, null, 2) + '\n');
+             });
+           }
+         })
+  )
+  .catch(e => handleError );
