4df4a7c42dfa8f23115a776e41144579d9956cc1
Dino Chiesa <dchiesa@google.com>
2021-02-03 13:50:08 -0800

    add more options to undeployAndMaybeDelete.js

--- undeployAndMaybeDelete.js
+++ undeployAndMaybeDelete.js
@@ -4,7 +4,7 @@
 // ------------------------------------------------------------------
 // undeploy and maybe delete an Apigee Edge proxy that has a name with a specific prefix.
 //
-// Copyright 2017-2020 Google LLC.
+// Copyright 2017-2021 Google LLC.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -18,7 +18,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 //
-// last saved: <2020-April-07 22:15:42>
+// last saved: <2021-February-03 13:47:33>
 /* jslint esversion:9 */
 
 const edgejs     = require('apigee-edge-js'),
@@ -26,13 +26,41 @@ const edgejs     = require('apigee-edge-js'),
       common     = edgejs.utility,
       apigeeEdge = edgejs.edge,
       Getopt     = require('node-getopt'),
-      version    = '20190211-1251',
+      version    = '20210203-1340',
       getopt     = new Getopt(common.commonOptions.concat([
-        ['P' , 'prefix=ARG', 'required. name prefix. Undeploy and maybe delete all API Proxies with names starting with this prefix.' ],
+        ['A' , 'all', 'optional. Undeploy (and optionally delete) all API Proxies.' ],
+        ['P' , 'prefix=ARG', 'optional. Undeploy (and optionally delete) API Proxies with names starting with this prefix.' ],
+        ['R' , 'regex=ARG', 'optional. Undeploy (and optionally delete) API Proxies with names that match this regex.' ],
         ['D' , 'delete', 'optional. Delete the proxies too. By default, just undeploy.' ]
       ])).bindHelp();
 
 // ========================================================
+
+function getNameMatcher(options) {
+  if ( ! options.prefix && ! options.all && ! options.regex) {
+    console.log('You must specify one of -P, -A, or -R');
+    getopt.showHelp();
+    process.exit(1);
+  }
+
+  if ((options.prefix && options.all) ||
+      (options.prefix && options.regex) ||
+      (options.all && options.regex)) {
+    console.log('You must specify at most one of -P, -A, or -R');
+    getopt.showHelp();
+    process.exit(1);
+  }
+
+  if (options.prefix)
+    return s => s.startsWith(options.prefix);
+  if (options.all)
+    return s => true;
+  if ( ! options.regex)
+    throw new Error('missing option');
+
+  let re = new RegExp(options.regex);
+  return s => s.match(re);
+}
 
 function revEnvReducer(org, name, revision) {
   return (p, deployment) => {
@@ -45,7 +73,7 @@ function revReducer(org, name) {
   return (p, revision) =>
     p.then( _ =>
             org.proxies.getDeployments({ name, revision })
-            // morph to support GAAMBO or legacy API
+            // to support GAAMBO or classic API
             .then( r => r.deployments || r.environment)
             .then( deployments => {
               //console.log('deployments: ' + util.format(deployments));
@@ -58,7 +86,7 @@ function revReducer(org, name) {
 function proxyReducer(org) {
   return (p, item) =>
     p.then( _ =>
-            // morph to support GAAMBO or legacy API
+            // to support GAAMBO or classicy API
             org.proxies.getRevisions({ name: item.name || item})
             .then( revisions =>
                    revisions.reduce(revReducer(org, item.name || item), Promise.resolve()))
@@ -73,11 +101,8 @@ common.logWrite('start');
 
 // process.argv array starts with 'node' and 'scriptname.js'
 var opt = getopt.parse(process.argv.slice(2));
-if ( ! opt.options.prefix ) {
-  console.log('You must specify a name prefix. (-P)');
-  getopt.showHelp();
-  process.exit(1);
-}
+
+let proxyMatcher = getNameMatcher(opt.options);
 
 common.verifyCommonRequiredParameters(opt.options, getopt);
 
@@ -86,9 +111,10 @@ apigeeEdge.connect(common.optToOptions(opt))
     org.proxies.get()
       .then( r => {
         //console.log('r: ' + JSON.stringify(r));
+        // to support GAAMBO or classic API
         let [collection, predicate] = (r.proxies) ?
-          [r.proxies, (p) => p.name.startsWith(opt.options.prefix)] :
-          [r, p => p.startsWith(opt.options.prefix)];
+          [r.proxies, p => proxyMatcher(p.name)] :
+          [r, p => proxyMatcher(p)];
 
         return collection
           .filter( p => predicate(p))
