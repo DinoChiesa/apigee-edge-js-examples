a98d680c1515acf14dcafe687788126144f99224
Dino Chiesa <dchiesa@google.com>
2021-02-03 18:37:13 -0800

    undeployAndMaybeDelete gets options: env and trial

--- undeployAndMaybeDelete.js
+++ undeployAndMaybeDelete.js
@@ -18,19 +18,21 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 //
-// last saved: <2021-February-03 13:47:33>
+// last saved: <2021-February-03 18:39:01>
 /* jslint esversion:9 */
 
-const edgejs     = require('apigee-edge-js'),
+const apigeejs   = require('apigee-edge-js'),
       util       = require('util'),
-      common     = edgejs.utility,
-      apigeeEdge = edgejs.edge,
+      common     = apigeejs.utility,
+      apigee     = apigeejs.edge,
       Getopt     = require('node-getopt'),
-      version    = '20210203-1340',
+      version    = '20210203-1839',
       getopt     = new Getopt(common.commonOptions.concat([
         ['A' , 'all', 'optional. Undeploy (and optionally delete) all API Proxies.' ],
         ['P' , 'prefix=ARG', 'optional. Undeploy (and optionally delete) API Proxies with names starting with this prefix.' ],
         ['R' , 'regex=ARG', 'optional. Undeploy (and optionally delete) API Proxies with names that match this regex.' ],
+        ['e' , 'env=ARG', 'optional. the environment(s) from which to undeploy. Separate multiple environments with a comma. default: all'],
+        ['t' , 'trial', 'trial only. Do not actually undeploy or delete'],
         ['D' , 'delete', 'optional. Delete the proxies too. By default, just undeploy.' ]
       ])).bindHelp();
 
@@ -62,14 +64,22 @@ function getNameMatcher(options) {
   return s => s.match(re);
 }
 
-function revEnvReducer(org, name, revision) {
+function revEnvReducer(options, org, envs, name, revision) {
   return (p, deployment) => {
     //console.log('deployment: ' + util.format(deployment));
-    return p.then( () => org.proxies.undeploy({name, revision, environment: deployment.environment || deployment.name}));
+    return p.then( _ => {
+      let environment = deployment.environment || deployment.name;
+      if (!envs || envs.indexOf(environment) >= 0) {
+        let args = {name, revision, environment};
+        return options.trial ? (console.log('WOULD UNDEPLOY ' + JSON.stringify(args)) && {}) :
+          org.proxies.undeploy(args);
+      }
+      return {};
+    });
   };
 }
 
-function revReducer(org, name) {
+function revReducer(options, org, envs, name) {
   return (p, revision) =>
     p.then( _ =>
             org.proxies.getDeployments({ name, revision })
@@ -78,19 +88,8 @@ function revReducer(org, name) {
             .then( deployments => {
               //console.log('deployments: ' + util.format(deployments));
               return (deployments && deployments.length > 0) ?
-                deployments.reduce(revEnvReducer(org, name, revision), Promise.resolve()) :
-                {};
+                deployments.reduce(revEnvReducer(options, org, envs, name, revision), Promise.resolve()) : {};
             }));
-}
-
-function proxyReducer(org) {
-  return (p, item) =>
-    p.then( _ =>
-            // to support GAAMBO or classicy API
-            org.proxies.getRevisions({ name: item.name || item})
-            .then( revisions =>
-                   revisions.reduce(revReducer(org, item.name || item), Promise.resolve()))
-            .then( _ => (opt.options.delete) ? org.proxies.del({ name: item.name || item}) : {} ));
 }
 
 console.log(
@@ -99,15 +98,14 @@ console.log(
 
 common.logWrite('start');
 
-// process.argv array starts with 'node' and 'scriptname.js'
-var opt = getopt.parse(process.argv.slice(2));
+let opt = getopt.parse(process.argv.slice(2));
 
 let proxyMatcher = getNameMatcher(opt.options);
 
 common.verifyCommonRequiredParameters(opt.options, getopt);
 
-apigeeEdge.connect(common.optToOptions(opt))
-  .then( (org) =>
+apigee.connect(common.optToOptions(opt))
+  .then( org =>
     org.proxies.get()
       .then( r => {
         //console.log('r: ' + JSON.stringify(r));
@@ -116,10 +114,27 @@ apigeeEdge.connect(common.optToOptions(opt))
           [r.proxies, p => proxyMatcher(p.name)] :
           [r, p => proxyMatcher(p)];
 
+        const envs = opt.options.env && opt.options.env.split(','); // env may be a comma-separated list
+
+        let proxyReducer = (p, item) =>
+        p.then( _ =>
+            // to support GAAMBO or classic API
+            org.proxies.getRevisions({ name: item.name || item})
+            .then( revisions =>
+                   revisions.reduce(revReducer(opt.options, org, envs, item.name || item), Promise.resolve()))
+                .then( _ => {
+                  if (opt.options.delete) {
+                    let args = { name: item.name || item};
+                    return opt.options.trial ? (console.log('WOULD DELETE ' + JSON.stringify(args)) && {}) :
+                      org.proxies.del(args);
+                  }
+                  return {};
+                }));
+
         return collection
           .filter( p => predicate(p))
-          .reduce(proxyReducer(org), Promise.resolve()) ;
+          .reduce(proxyReducer, Promise.resolve()) ;
       })
-      .then( (results) => common.logWrite('all done...') )
+      .then( results => common.logWrite('all done...') )
   )
   .catch( e => console.log(util.format(e)));
