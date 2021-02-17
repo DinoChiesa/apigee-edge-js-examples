fc143517048d01eb7612805d67ebd4f3a7a15e16
Dino Chiesa <dchiesa@google.com>
2021-02-17 13:15:17 -0800

    less logging for getToken in non-verbose mode

--- getToken.js
+++ getToken.js
@@ -2,9 +2,10 @@
 /*jslint node:true */
 // getToken.js
 // ------------------------------------------------------------------
-// authenticate, get a token suitable for use with the Apigee Edge Admin API.
+// authenticate, get a token suitable for use with the Apigee Admin API.
+// This works with Apigee X; use a service-account key file.
 //
-// Copyright 2019-2020 Google LLC.
+// Copyright 2019-2021 Google LLC.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -18,45 +19,44 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 //
-// last saved: <2020-December-16 09:48:54>
+// last saved: <2021-February-17 13:14:58>
 
-const edgejs     = require('apigee-edge-js'),
-      common     = edgejs.utility,
-      apigeeEdge = edgejs.edge,
+const apigeejs   = require('apigee-edge-js'),
+      common     = apigeejs.utility,
+      apigee     = apigeejs.edge,
       Getopt     = require('node-getopt'),
-      version    = '20200511-1133',
+      version    = '20210217-1312',
       getopt     = new Getopt(common.commonOptions).bindHelp();
 
-console.log(
-  'Edge Get Token, version: ' + version + '\n' +
-    'Node.js ' + process.version + '\n');
+let opt = getopt.parse(process.argv.slice(2));
 
-var opt = getopt.parse(process.argv.slice(2));
+if (opt.options.verbose) {
+  console.log(
+    'Edge Get Token, version: ' + version + '\n' +
+      'Node.js ' + process.version + '\n');
+}
 
 common.verifyCommonRequiredParameters(opt.options, getopt);
 
-// apigeeEdge.connect() will connect by using an existing token, or by
+// apigee.connect() will connect by using an existing token, or by
 // getting a new token, depending on the passed-in options as well as
 // the state of any existing (cached) access_token and refresh_token.
-apigeeEdge.connect(common.optToOptions(opt))
+apigee.connect(common.optToOptions(opt))
   .then( org =>
       org.conn.getExistingToken()
       .then( existingToken => {
-        if (opt.options.verbose) {
-          console.log();
-        }
-        console.log(existingToken.access_token);
+        console.log('\n' + existingToken.access_token);
         if (opt.options.verbose) {
           let jwt = existingToken.access_token,
               jwtparts = jwt.split(new RegExp('\\.')),
               payload = Buffer.from(jwtparts[1], 'base64').toString('utf-8');
           try {
-              let claims = JSON.parse(payload);
-          console.log( '\nissuer   : ' + claims.iss);
-          console.log( 'user     : ' + claims.user_name);
-          console.log( 'issued at: ' + (new Date(claims.iat * 1000)).toISOString());
-          console.log( 'expires  : ' + (new Date(claims.exp * 1000)).toISOString());
-          console.log( 'now      : ' + (new Date()).toISOString());
+            let claims = JSON.parse(payload);
+            console.log( '\nissuer   : ' + claims.iss);
+            console.log( 'user     : ' + claims.user_name);
+            console.log( 'issued at: ' + (new Date(claims.iat * 1000)).toISOString());
+            console.log( 'expires  : ' + (new Date(claims.exp * 1000)).toISOString());
+            console.log( 'now      : ' + (new Date()).toISOString());
             console.log( 'client_id: ' + claims.client_id);
           }
           catch (e) {
