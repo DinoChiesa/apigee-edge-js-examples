d9595a6b7c82f318d2d2debfb4c2b77f072b42ca
Dino Chiesa <dchiesa@google.com>
2021-04-21 17:09:17 -0700

    convert listAndQueryDevelopers to use promises

--- listAndQueryDevelopers.js
+++ listAndQueryDevelopers.js
@@ -4,7 +4,7 @@
 // ------------------------------------------------------------------
 // list and query developers in Apigee Edge
 //
-// Copyright 2017-2019 Google LLC.
+// Copyright 2017-2021 Google LLC.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -18,66 +18,54 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 //
-// last saved: <2019-February-11 12:46:53>
+// last saved: <2021-April-21 17:07:53>
 
-var edgejs = require('apigee-edge-js'),
-    common = edgejs.utility,
-    apigeeEdge = edgejs.edge,
-    async = require('async'),
-    sprintf = require('sprintf-js').sprintf,
-    Getopt = require('node-getopt'),
-    version = '20190211-1246',
-    getopt = new Getopt(common.commonOptions.concat([
-      ['E' , 'expand', 'expand for each developer']
-    ])).bindHelp();
+const apigeejs = require('apigee-edge-js'),
+      common   = apigeejs.utility,
+      apigee   = apigeejs.edge,
+      util     = require('util'),
+      sprintf  = require('sprintf-js').sprintf,
+      Getopt   = require('node-getopt'),
+      version  = '20210421-1701',
+      getopt   = new Getopt(common.commonOptions.concat([
+          ['E' , 'expand', 'optional. expand for each developer']
+      ])).bindHelp();
 
 // ========================================================
-
-console.log(
-  'Apigee Edge Developer query tool, version: ' + version + '\n' +
-    'Node.js ' + process.version + '\n');
-
-common.logWrite('start');
-
 // process.argv array starts with 'node' and 'scriptname.js'
-var opt = getopt.parse(process.argv.slice(2));
+let opt = getopt.parse(process.argv.slice(2));
+
+if (opt.options.verbose) {
+    console.log(
+        `Apigee Developer query tool, version: ${version}\n` +
+        `Node.js ${process.version}\n`);
+
+    common.logWrite('start');
+}
+
 common.verifyCommonRequiredParameters(opt.options, getopt);
 
-apigeeEdge.connect(common.optToOptions(opt), function(e, org) {
-  if (e) {
-    common.logWrite(JSON.stringify(e, null, 2));
-    if (result) {
-      common.logWrite(JSON.stringify(result, null, 2));
-    }
-    process.exit(1);
-  }
-  common.logWrite('connected');
+apigee
+    .connect(common.optToOptions(opt))
+    .then( org => {
+        common.logWrite('connected');
 
-  org.developers.get({}, function(e, result){
-    if (e) {
-      common.logWrite(JSON.stringify(e, null, 2));
-      common.logWrite(JSON.stringify(result, null, 2));
-      process.exit(1);
-    }
-    common.logWrite(sprintf('developers: %s', JSON.stringify(result, null, 2)));
-    if (opt.options.expand && Array.isArray(result)) {
+        return org.developers.get({})
+            .then( devs => {
+                common.logWrite(sprintf('developers: %s', JSON.stringify(devs, null, 2)));
+                if (opt.options.expand && Array.isArray(devs)) {
 
-      var inquireOneDev = function(devEmail, cb) {
-            org.developers.get({developerEmail: devEmail}, function(e, result) {
-              if (e) { return cb(e); }
-              return cb(null, result);
+                    let r = (p, developerEmail) =>
+                    p.then( acc =>
+                            org.developers.get({ developerEmail })
+                            .then( result => [...acc, result]));
+
+                    return devs
+                        .reduce(r, Promise.resolve([]))
+                        .then(results => {
+                            console.log(JSON.stringify(results, null, 2) + '\n');
+                        });
+                }
             });
-          };
-
-      async.map(result, inquireOneDev, function (e, results) {
-        if (e) {
-          common.logWrite(JSON.stringify(e, null, 2));
-          common.logWrite(JSON.stringify(results, null, 2));
-        }
-        else {
-          common.logWrite(JSON.stringify(results, null, 2));
-        }
-      });
-    }
-  });
-});
+    })
+  .catch( e => console.log('while executing, error: ' + util.format(e)) );
