b1a3ad894ec68528c83df6be9f30bd930bae0062
Dino Chiesa <dchiesa@google.com>
2022-03-30 15:47:40 -0700

    update createKeyStore.js to handle keystore exists

--- createKeystore.js
+++ createKeystore.js
@@ -2,13 +2,16 @@
 /*jslint node:true */
 // createKeystore.js
 // ------------------------------------------------------------------
-// provision a keystore with a key and cert in Apigee Edge, and create a reference
+// provision a keystore with a key and cert in Apigee, and create a reference
 // to it. Or, provision a keystore as truststore, and upload a cert to it.
 //
 // example usage:
-// node ./createKeystore.js -v -n -o $ORG -s $KEYSTORE -e $ENV -k ./dchiesa.net.key  -c ./dchiesa.net.cert -a alias1
+// node ./createKeystore.js --apigeex --token $TOKEN
+//     --keypemfile ./example-private-key-20220330-1543.pem
+//     --certpemfile ./example-cert-20220330-1543.pem
+//     -o $ORG -e $ENV --keystore keystore1 --alias alias1
 //
-// Copyright 2017-2019 Google LLC.
+// Copyright 2017-2022 Google LLC.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -22,22 +25,22 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 //
-// last saved: <2019-November-25 08:50:11>
+// last saved: <2022-March-30 15:47:22>
 
-const edgejs     = require('apigee-edge-js'),
+const apigeejs   = require('apigee-edge-js'),
       fs         = require('fs'),
       util       = require('util'),
-      common     = edgejs.utility,
-      apigeeEdge = edgejs.edge,
+      utility    = apigeejs.utility,
+      apigee     = apigeejs.apigee,
       sprintf    = require('sprintf-js').sprintf,
       Getopt     = require('node-getopt'),
-      version    = '20191125-0850',
-      getopt     = new Getopt(common.commonOptions.concat([
+      version    = '20220330-1535',
+      getopt     = new Getopt(utility.commonOptions.concat([
         ['e' , 'environment=ARG', 'required. environment in which the keystore will be created'],
         ['s' , 'keystore=ARG', 'optional. name of the keystore to create. default: a generated random name'],
-        ['c' , 'certfile=ARG', 'required. path to the cert file'],
-        ['a' , 'alias=ARG', 'required. alias for the key'],
-        ['k' , 'keyfile=ARG', 'optional. path to the key file (PEM format)'],
+        ['' , 'certpemfile=ARG', 'required. path to the cert file'],
+        ['' , 'alias=ARG', 'required. alias for the key'],
+        ['' , 'keypemfile=ARG', 'optional. path to the key file (PEM format)'],
         ['P' , 'keypassword=ARG', 'optional. password for the RSA Key'],
         ['R' , 'reference=ARG', 'optional. reference to create or update']
       ])).bindHelp();
@@ -45,10 +48,10 @@ const edgejs     = require('apigee-edge-js'),
 // ========================================================
 
 console.log(
-  'Apigee Edge Keystore/Truststore creation tool, version: ' + version + '\n' +
-    'Node.js ' + process.version + '\n');
+  `Apigee Keystore/Truststore creation tool, version: ${version}\n` +
+    `Node.js ${process.version}\n`);
 
-common.logWrite('start');
+utility.logWrite('start');
 
 // process.argv array starts with 'node' and 'scriptname.js'
 var opt = getopt.parse(process.argv.slice(2));
@@ -63,18 +66,18 @@ if ( !opt.options.keystore ) {
   // contrive a name
   opt.options.keystore = new Date().toISOString(). replace(/-/g, '').substring(0,8) + '-' +
     Math.random().toString(36).substring(2, 15);
-  common.logWrite('using keystore: %s', opt.options.keystore);
+  utility.logWrite('using keystore: %s', opt.options.keystore);
 }
 
-if ( opt.options.keyfile ) {
-  if (!fs.existsSync(opt.options.keyfile))  {
-    console.log('You must specify a path to a valid file for the keyfile');
+if ( opt.options.keypemfile ) {
+  if (!fs.existsSync(opt.options.keypemfile))  {
+    console.log('You must specify a path to a valid file for the keypemfile');
     getopt.showHelp();
     process.exit(1);
   }
 }
 
-if ( !opt.options.certfile || !fs.existsSync(opt.options.certfile) ) {
+if ( !opt.options.certpemfile || !fs.existsSync(opt.options.certpemfile) ) {
   console.log('You must specify a path to a cert file');
   getopt.showHelp();
   process.exit(1);
@@ -86,71 +89,91 @@ if ( !opt.options.alias ) {
   process.exit(1);
 }
 
-common.verifyCommonRequiredParameters(opt.options, getopt);
-apigeeEdge.connect(common.optToOptions(opt))
+utility.verifyCommonRequiredParameters(opt.options, getopt);
+apigee.connect(utility.optToOptions(opt))
   .then( org => {
     if (opt.options.verbose) {
-      common.logWrite('connected');
+      utility.logWrite('connected');
     }
-    const options = {
-            environment : opt.options.environment,
-            name : opt.options.keystore
-          };
-    return org.keystores.create(options)
-      .then( result => {
-        if (opt.options.verbose) {
-          common.logWrite('created keystore %s', opt.options.keystore);
+    return org.keystores.get({ environment : opt.options.environment })
+      .then(result => {
+        let options = {
+              environment : opt.options.environment,
+              name : opt.options.keystore
+            };
+        let p = null;
+        if (result.indexOf(opt.options.keystore)>=0) {
+          // exists
+          p = org.keystores.getAliases(options)
+            .then( result => {
+              //utility.logWrite('keystore getAliases %s', JSON.stringify(result));
+              if (result.indexOf(opt.options.alias)>=0) {
+                return Promise.reject('that alias already exists.');
+              }
+            });
         }
-        options.certFile = opt.options.certfile;
-        options.alias = opt.options.alias;
-        if ( opt.options.keyfile ) {
-          options.keyFile = opt.options.keyfile;
-          if (opt.options.keypassword) {
-            options.keyPassword = opt.options.keypassword;
+        else {
+          // does not exist, create it
+          p =  org.keystores.create(options)
+            .then( result => {
+              if (opt.options.verbose) {
+                utility.logWrite('created keystore %s', opt.options.keystore);
+              }
+            });
+        }
+
+        return p.then( _ => {
+          options.certFile = opt.options.certpemfile;
+          options.alias = opt.options.alias;
+          if ( opt.options.keypemfile ) {
+            options.keyFile = opt.options.keypemfile;
+            if (opt.options.keypassword) {
+              options.keyPassword = opt.options.keypassword;
+            }
           }
-        }
-        return org.keystores.importCert(options)
-          .then(result => {
-            if (opt.options.verbose) {
-              common.logWrite('%scert stored.', ( opt.options.keyfile) ? "key and " :"");
-            }
-            if ( ! opt.options.reference) {
-              const o = {
-                      org: org.conn.orgname,
-                      env: opt.options.environment,
-                      keystore: opt.options.keystore,
-                      ref: '-none-',
-                      now: (new Date()).toISOString()
-                    };
-              if ( opt.options.keyfile ) {
-                o.keyalias = opt.options.alias;
+          return org.keystores.importCert(options)
+            .then(result => {
+              if (opt.options.verbose) {
+                utility.logWrite('%scert stored.', ( opt.options.keypemfile) ? "key and " :"");
               }
-              console.log('\nsummary: ' + JSON.stringify(o, null, 2));
-              return Promise.resolve(true);
-            }
-            const options = {
-                    name : opt.options.reference,
-                    refers : opt.options.keystore,
-                    environment : opt.options.environment
-                  };
-            return org.references.createOrUpdate(options)
-              .then( result => {
-                if (opt.options.verbose) {
-                  common.logWrite('reference %s created or updated.', opt.options.reference);
-                  const o = {
-                          org: org.conn.orgname,
-                          env: opt.options.environment,
-                          keystore: opt.options.keystore,
-                          ref: opt.options.reference,
-                          now: (new Date()).toISOString()
-                        };
-              if ( opt.options.keyfile ) {
-                o.keyalias = opt.options.alias;
-              }
-                  console.log('\nsummary: ' + JSON.stringify(o, null, 2));
+              if ( ! opt.options.reference) {
+                const o = {
+                        org: org.conn.orgname,
+                        env: opt.options.environment,
+                        keystore: opt.options.keystore,
+                        ref: '-none-',
+                        now: (new Date()).toISOString()
+                      };
+                if ( opt.options.keypemfile ) {
+                  o.keyalias = opt.options.alias;
                 }
-              });
-          });
+                console.log('\nsummary: ' + JSON.stringify(o, null, 2));
+                return Promise.resolve(true);
+              }
+              const options = {
+                      name : opt.options.reference,
+                      refers : opt.options.keystore,
+                      environment : opt.options.environment
+                    };
+              return org.references.createOrUpdate(options)
+                .then( result => {
+                  if (opt.options.verbose) {
+                    utility.logWrite('reference %s created or updated.', opt.options.reference);
+                    const o = {
+                            org: org.conn.orgname,
+                            env: opt.options.environment,
+                            keystore: opt.options.keystore,
+                            ref: opt.options.reference,
+                            now: (new Date()).toISOString()
+                          };
+                    if ( opt.options.keypemfile ) {
+                      o.keyalias = opt.options.alias;
+                    }
+                    console.log('\nsummary: ' + JSON.stringify(o, null, 2));
+                  }
+                });
+            });
+        });
       });
   })
   .catch(e => console.log(util.format(e)));
