d79f2411b83edc7564b254d76870c8d8effb4620
DinoChiesa <dchiesa@google.com>
2025-01-15 04:05:37 +0000

    fix: update listApiDeployments to work with Apigee X/hybrid

--- listApiDeployments.js
+++ listApiDeployments.js
@@ -2,9 +2,10 @@
 /*jslint node:true */
 // listApiDeployments.js
 // ------------------------------------------------------------------
-// list deployments for a particular environment in Apigee
+// list deployments for a particular Apigee environment, or
+// all environments in an Apigee organization.
 //
-// Copyright 2017-2022 Google LLC.
+// Copyright 2017-2022, 2025 Google LLC.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -18,44 +19,85 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 //
-// last saved: <2022-May-11 11:17:35>
+// last saved: <2025-January-15 04:05:15>
 
-const apigeejs   = require('apigee-edge-js'),
-      util       = require('util'),
-      common     = apigeejs.utility,
-      apigee     = apigeejs.apigee,
-      sprintf    = require('sprintf-js').sprintf,
-      Getopt     = require('node-getopt'),
-      version    = '20220511-1117',
-      getopt     = new Getopt(common.commonOptions.concat([
-          ['e' , 'environment=ARG', 'Optional. inquire and show the deployments for a particular environment.'],
-      ])).bindHelp();
+const apigeejs = require("apigee-edge-js"),
+  util = require("util"),
+  common = apigeejs.utility,
+  apigee = apigeejs.apigee,
+  sprintf = require("sprintf-js").sprintf,
+  Getopt = require("node-getopt"),
+  version = "20220511-1117",
+  getopt = new Getopt(
+    common.commonOptions.concat([
+      [
+        "e",
+        "environment=ARG",
+        "Optional. inquire and show the deployments for a particular environment.",
+      ],
+    ]),
+  ).bindHelp();
+
+function getDeployments(org, environment) {
+  return org.getApiDeployments({ environment }).then((resp) => {
+    // GAAMBO or Edge
+    if (resp.deployments) {
+      return resp.deployments.map((d) => {
+        delete d.proxyDeploymentType;
+        return d;
+      });
+    }
+    if (resp.aPIProxy) {
+      return resp.aPIProxy.map((p) => ({
+        name: p.name,
+        revision: p.revision.map((r) => ({
+          configuration: r.configuration,
+          name: r.name,
+          state: r.state,
+        })),
+      }));
+    }
+
+    throw new Error("unexpected response format");
+  });
+}
 
 // ========================================================
 
 console.log(
   `Apigee API Deployments List tool, version: ${version}\n` +
-    `Node.js ${process.version}\n`);
+    `Node.js ${process.version}\n`,
+);
 
-common.logWrite('start');
+common.logWrite("start");
 
 // process.argv array starts with 'node' and 'scriptname.js'
 var opt = getopt.parse(process.argv.slice(2));
 common.verifyCommonRequiredParameters(opt.options, getopt);
 
-apigee.connect(common.optToOptions(opt))
-  .then(org =>
-    org.getApiDeployments({environment:opt.options.environment})
-        .then(resp => {
-          // GAAMBO or Edge
-          let proxies = (resp.aPIProxy) ?
-            resp.aPIProxy.map(p => ({
-              name:p.name,
-              revision: p.revision.map( r => ({configuration: r.configuration, name: r.name, state: r.state}) )
-            })) :
-            resp;
-          common.logWrite(sprintf('found %d proxies', proxies.length));
-          return proxies;
-        }))
-  .then(results => console.log('results: ' + JSON.stringify(results, null, 2)))
-  .catch( e => console.error('error: ' + util.format(e) ));
+apigee
+  .connect(common.optToOptions(opt))
+  .then((org) => {
+    if (opt.options.environment) {
+      return getDeployments(org, opt.options.environment);
+    }
+
+    //  else, no environment specified. Query environments and retrieve all.
+    return org.environments.get().then((resp) => {
+      const reducer = (promise, item) =>
+        promise.then((a) =>
+          getDeployments(org, item).then((deployments) => {
+            //console.log(JSON.stringify(deployments));
+            return [...a, ...deployments];
+          }),
+        );
+      return resp.reduce(reducer, Promise.resolve([]));
+    });
+  })
+  .then((results) => {
+    common.logWrite(sprintf("found %d deployments", results.length));
+    if (opt.verbose) {
+      console.log("results: " + JSON.stringify(results, null, 2));
+    }
+  })
+  .catch((e) => console.error("error: " + util.format(e)));
